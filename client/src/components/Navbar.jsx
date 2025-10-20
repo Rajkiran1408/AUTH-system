6488e6351228c37afcd6d8633e2cf757afa97910
Rajkiran1408 <rameshsundarraj72@gmail.com>
2025-10-20 11:22:26 +0530

    change origin

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const sendVerificationOtp = async ()=>{
     try {
       axios.defaults.withCredentials=true;
-      const {data} = await axios.post(backendUrl + '/api/auth/send-verify-otp')
+      const {data} = await axios.post(`${backendUrl}/api/auth/send-verify-otp`)
       if(data.success){
         navigate('/email-verify')
         toast.success(data.message)
@@ -29,7 +29,7 @@ const Navbar = () => {
   const logout = async ()=>{
     try {
       axios.defaults.withCredentials=true
-      const {data} = await axios.post(backendUrl + '/api/auth/logout')
+      const {data} = await axios.post(`${backendUrl}/api/auth/logout`)
       data.success && setIsLoggedin(false)
       data.success && setUserData(null);
       navigate('/')
