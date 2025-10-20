4687e264af82408868dcdd54213442ac92b1f434
Rajkiran1408 <rameshsundarraj72@gmail.com>
2025-10-20 11:01:01 +0530

    change origin

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const allowedOrigins = ['http://localhost:5173','https://auth-system-aatr.onrend
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin:allowedOrigins , credentials:true}));
+app.options('*', cors({ origin: allowedOrigins, credentials: true })); 
 
 
 
